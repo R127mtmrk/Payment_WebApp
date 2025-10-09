90552dad65247953bbddaa88ed3568c3781acde5
clem-clem06 <clement.julien@iia-formation.fr>
2025-10-09 11:38:20 +0200

    creation des pages connexion et inscription

---
+++ Views/connexion.php
@@ -0,0 +1,29 @@
+<?php
+require '../Controller/connexion.php';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+    <!--  CSS <link rel="stylesheet" href=""> -->
+</head>
+<body>
+<div class="container">
+    <h2>Connexion</h2>
+    <form action="#" method="POST">
+        <div class="form-group">
+            <label for="username">Nom d'utilisateur</label>
+            <input type="text" id="username" name="username" required>
+        </div>
+        <div class="form-group">
+            <label for="password">Mot de passe</label>
+            <input type="password" id="password" name="password" required>
+        </div>
+        <button type="submit">Se connecter</button>
+    </form>
+    <p>Pas encore de compte ? <a href="inscription.php">Inscrivez-vous ici</a></p>
+</div>
+</body>
+</html>
---
+++ Views/inscription.php
@@ -0,0 +1,29 @@
+<?php
+require '../Controller/inscription.php';
+?>
+    <!DOCTYPE html>
+    <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Inscription</title>
+        <!--  CSS <link rel="stylesheet" href=""> -->
+    </head>
+    <body>
+    <div class="container">
+        <h2>Inscription</h2>
+        <form action="#" method="POST">
+            <div class="form-group">
+                <label for="username">Nom d'utilisateur</label>
+                <input type="text" id="username" name="username" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Mot de passe</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <button type="submit">S'inscrire</button>
+        </form>
+        <p>Déjà un compte ? <a href="connexion.php">Connectez-vous ici</a></p>
+    </div>
+    </body>
+    </html>
