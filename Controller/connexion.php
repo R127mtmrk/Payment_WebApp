d91092af5f86b9fecc2ddb02c9061da117aefee8
Baltroumark <ylan.moulard@gmail.com>
2025-10-09 12:28:37 +0200

    SQL modif

--- Controller/connexion.php
+++ Controller/connexion.php
@@ -4,6 +4,4 @@ require_once '../SQL_Request/Select.php';
 
 $username = htmlspecialchars($_POST['username']);
 $password = htmlspecialchars($_POST['password']);
-$table = "users";
-
 $check = ConnectSelect($username, $password); // si check est à True, l'utilisateur peut être connecté
