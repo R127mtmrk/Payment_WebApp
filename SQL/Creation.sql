585941a9b9ff1411cc476d76e2ff9799a159ae73
Baltroumark <ylan.moulard@gmail.com>
2025-10-09 09:55:03 +0200

    BDD start Creation.sql

--- SQL/Creation.sql
+++ SQL/Creation.sql
@@ -2,12 +2,13 @@ CREATE DATABASE Payment_WebApp CHARACTER SET 'utf8mb4';
 USE Payment_WebApp;
 
 CREATE TABLE Debit_Cards (
-
-)
+    id_card INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    num_card CHAR(16) NOT NULL,
+);
 
 CREATE TABLE Users (
     id_user INT UNSIGNED NOT NULL AUTO_INCREMENT,
     psd_user VARCHAR(50) NOT NULL,
     email_user VARCHAR(100) NOT NULL,
     id_card_user
-)
+);
