1ed7d65aadf60873d74ae0ef48fabe94e02ccbc0
clem-clem06 <clement.julien@iia-formation.fr>
2025-11-25 17:13:36 +0100

    l'index bloque les donnee corrige

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
 ini_set('session.cookie_httponly', 1);
+
 session_start();
-if (isset($_SESSION['connected']) && function_exists($_SESSION['connected'])) {
+
+if (isset($_SESSION['connected']) && $_SESSION['connected'] === true) {
     header('Location: Controller/dashboard.php');
     exit();
-}else{
+} else {
     header('Location: Controller/connexion.php');
     exit();
 }
+?>
