2c4cfdfb84c4249b34730629175bc021737e3caf
clem-clem06 <clement.julien@iia-formation.fr>
2025-11-28 05:50:51 +0100

    ajout de secu et fonction deplace

--- Controller/account_suppression.php
+++ Controller/account_suppression.php
@@ -1,17 +1,26 @@
 <?php
+require_once 'init.php';
 require_once '../SQL_Request/Delete.php';
-require_once 'cookie_param.php';
-session_start();
 
-if (isset($_SESSION['connected']) && $_SESSION['connected'] === true) {
+$errorMessage = "";
 
+redirectIfConnected();
+
+redirectIfAdmin();
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (!isset($_POST['csrf_token']) || $_POST['csrf_token'] !== $_SESSION['csrf_token']) {
+            die("Erreur CSRF : Requête non autorisée.");
+        }
+
+        $password = $_POST['password'] ?? '';
+
+        if (DeleteAccount($_SESSION['username'], $password)) {
+            session_destroy();
+            header("Location: ../index.php?bye=1");
+            exit();
+        } else {
+            $errorMessage = "Suppression échouée. Vérifiez votre mot de passe.";
+        }
+    }
     require '../views/account_suppression.php';
-    require_once '../SQL_Request/Select.php';
-
-    $password = isset($_POST['password']) ? htmlspecialchars($_POST['password']) : '';
-    DeleteAccount($_SESSION['username'], $password);
-
-    }else{
-    // Envoi du header 404 Not Found
-    require '../Views/404.php';
-}
