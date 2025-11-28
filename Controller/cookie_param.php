31656ae8f7bf7ea173bc60cb8d7b2c31f3b804a2
clem-clem06 <clement.julien@iia-formation.fr>
2025-11-28 05:57:55 +0100

    ajout secu

--- Controller/cookie_param.php
+++ Controller/cookie_param.php
@@ -3,3 +3,5 @@ ini_set('session.cookie_httponly', 1);
 ini_set('session.use_strict_mode', 1);
 ini_set('session.use_only_cookies', 1);
 ini_set('session.cookie_lifetime', 0);
+ini_set('session.cookie_secure', 1);
+ini_set('session.cookie_samesite', 'Strict');
